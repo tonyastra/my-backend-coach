74c4feb315aafa92a97a15e50d5212045ac205d0
tonyastra <spidertonygbm@hotmail.fr>
2025-06-27 10:44:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // 🔧 Configs
 const USERS_FILE = path.join(__dirname, 'users.json');
